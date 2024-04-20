4f026d3ca4d6f8adb524c20750cb5bbdf3641641
Ymk2021 <167631045+Ymk2021@users.noreply.github.com>
2024-04-20 23:54:24 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "Enter the URL from email: https://download6.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoidTZ3c2sxMjJlbnUzZHphbzg4YW80bzVmIiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQ2LmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTM3MDg5OTJ9fX1dfQ__&Signature=rzxvEkjgu8LI3M3f%7E87F5U9CWkHNIvuB6D32JkWetP3BnQcBMdWQfEietsyWUAhPi0nrqosbCSxsg8uEMG-J%7EA31qg0HbBBfBHZw9OPtI-o3qSGQgDf-18DHS6Sr627flJjsFpbXyx9if4x9liI7asUhsWW3%7EvVrgNoP1RcKAEMqNTHHSYW7Abh%7Ebh%7EemyB36DZRSEINxvzoCTzy6rmC2FACJ4W27Cmqa2rju-I9eJtglhQMMQ9aU-HTJT-QFdOaf-Z6U3z-JiHgFVtD-OxUU4-M6yxosOGS3z0-1X-ISs1DZqzJVYudivz5XES81EpcjByhD1jHRrBmLOWVyqn3oQ__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=389020143962634" PRESIGNED_URL
 echo ""
 read -p "Enter the list of models to download without spaces (8B,8B-instruct,70B,70B-instruct), or press Enter for all: " MODEL_SIZE
 TARGET_FOLDER="."             # where all files should end up
